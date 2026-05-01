ad1a3df6a30a2b88f6c2aadcfc3d8973eb85081c
Juan Suárez <suarezramirezjuanjose@hotmail.com>
2026-05-01 12:30:45 +0200

    feat(env): add supabase config to envs

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://rrrgvoxenaabqymhtpqd.supabase.co',
+  supabaseAnonKey:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJycmd2b3hlbmFhYnF5bWh0cHFkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc2MzA2OTEsImV4cCI6MjA5MzIwNjY5MX0.3JjEqImWGM0GZLSEa2-YxCvPDq38NALI-Qv1UIBHcCI',
+  stripePublicKey: 'pk_test_...',
+  appUrl: 'http://localhost:4200',
+};
